c209fe5cb99a213e12f201ce83481932e573df49
Mark Kruse <mkruse@gmail.com>
2011-11-27 21:55:45 -0600

    Initial commit of spaghetti code

---
+++ Editor/CGrid.vb
@@ -0,0 +1,14 @@
+﻿Public Class CGrid
+    Public Data(1, 1) As Color
+    Public FrameNumber As Integer
+
+    Public Sub Init(ByVal f As Integer, ByVal x As Integer, ByVal y As Integer)
+        ReDim Data(x, y)
+        FrameNumber = f
+        For xc As Integer = 0 To x
+            For yc As Integer = 0 To y
+                Data(xc, yc) = Color.Black
+            Next
+        Next
+    End Sub
+End Class
